870c2930fea7fa83da5922e3214e2c4ab098e3a5
duyvu265 <duyvu26597@example.com>
2024-10-15 11:20:44 +0700

    add is_active in product and user

--- Products/models.py
+++ Products/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from ProductsCategory.models import ProductCategory
 
-# Create your models here.
-# Mô hình sản phẩm
 class Product(models.Model):
     category = models.ForeignKey(ProductCategory, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
@@ -12,5 +10,6 @@ class Product(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     price = models.DecimalField(max_digits=10, decimal_places=2) 
     quantity = models.PositiveIntegerField(default=0)
+    is_active = models.BooleanField(default=False)
     def __str__(self):
         return self.name
--- SiteUser/models.py
+++ SiteUser/models.py
@@ -7,7 +7,7 @@ class SiteUser(models.Model):
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    is_active = models.BooleanField(default=False)
     def __str__(self):
         return self.user.username
 
